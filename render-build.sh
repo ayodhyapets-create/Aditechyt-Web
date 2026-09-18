be7992919a782a211811c61226b5bf5113903a9b
ayodhyapets-create <ayodhyapets@gmail.com>
2026-09-18 12:04:07 +0530

    Modify render-build.sh for Python and FFmpeg setup
    Updated script to install Python packages and download FFmpeg.

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
-# Create bin directory
-mkdir -p bin
+# 1. Python packages install karein
+pip install -r requirements.txt
 
-# Download FFmpeg static build
-curl -O https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-tar -xf ffmpeg-release-amd64-static.tar.xz
+# 2. FFmpeg static binary download karein
+echo "Downloading FFmpeg..."
+mkdir -p bin
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
 find ffmpeg-*-static -type f -name "ffmpeg" -exec cp {} bin/ \;
 find ffmpeg-*-static -type f -name "ffprobe" -exec cp {} bin/ \;
-rm -rf ffmpeg-*-static *.tar.xz
-
-# Make it accessible in PATH
-export PATH="$PWD/bin:$PATH"
+rm -rf ffmpeg-*-static ffmpeg.tar.xz
+chmod +x bin/ffmpeg bin/ffprobe
