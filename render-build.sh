943c4f94209bcb3c1ba1af196423e053a3e8cafd
ayodhyapets-create <ayodhyapets@gmail.com>
2026-09-20 12:23:00 +0530

    Update FFmpeg download method in render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,10 +4,10 @@ set -o errexit
 # 1. Python packages install karein
 pip install -r requirements.txt
 
-# 2. Direct zip format mein FFmpeg download karein (No xz tool needed)
-echo "Downloading FFmpeg..."
+# 2. Pre-compiled static ffmpeg binary install karein
 mkdir -p bin
-curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o ffmpeg.tar.xz
+curl -L -o bin/ffmpeg https://github.com/eugeneware/ffmpeg-static/releases/latest/download/linux-x64
+chmod +x bin/ffmpeg
 
-# Alternatively, lets use a zip or pre-compiled static binary url or we can just skip ffmpeg if yt-dlp basic works, 
-# but let's use a reliable direct static link:
+# PATH export karein taaki yt-dlp ko direct mil jaye
+export PATH="$(pwd)/bin:$PATH"
